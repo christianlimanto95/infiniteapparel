96abc831b2cb3d88c9464f17a325e548d4b2b2f0
christianlimanto95 <christianlimanto95@gmail.com>
2017-11-30 14:35:47 +0700

    major fix

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,6 +1,22 @@
 <html>
 <head>
 	<title><?php echo $title; ?></title>
+	<style>
+		@font-face {
+			font-family: champagne;
+			src: url(<?php echo base_url("assets/fonts/champagne.ttf"); ?>);
+		}
+
+		@font-face {
+			font-family: pier-bold;
+			src: url(<?php echo base_url("assets/fonts/pier-bold.otf"); ?>);
+		}
+
+		@font-face {
+			font-family: pier-regular;
+			src: url(<?php echo base_url("assets/fonts/pier-regular.otf"); ?>);
+		}
+	</style>
 	
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/common/default.css"); ?>" />
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/" . $page_name . ".css"); ?>" />
@@ -9,5 +25,22 @@
 </head>
 <body>
 <div class="header">
+	<div class="header-inner">
+		<a href="#" class="logo" style="background-image: url(assets/icons/logo.png);"></a>
+		<div class="header-menu-container">
+			<a href="#" class="header-menu">HOME</a>
+			<a href="#" class="header-menu">CATALOG</a>
+			<a href="#" class="header-menu">CONTACT</a>
+		</div>
+	</div>
+	<div class="bags-container">
+		<div class="bags-container-inner">
+			<div class="bags-image" style="background-image: url(assets/icons/bag.png);"></div>
+			<div class="bags-ctr">1</div>
+		</div>
+	</div>
+	<div class="header-login-or-signup">
+		<span>Login</span> or <span>Sign Up Yours</span>
+	</div>
 </div>
 <div class="container">
--- application/views/home.php
+++ application/views/home.php
@@ -1,2 +1,19 @@
 <div class="content">
+    <div class="section section-1" style="background-image: url(assets/images/home.jpg);">
+        <div class="section-gradient"></div>
+        <a href="#" class="explore-products-container">
+            <div class="explore-products-image" style="background-image: url(assets/images/explore.png);"></div>
+            <div class="btn-explore-products">EXPLORE PRODUCTS</div>
+        </a>
+    </div>
+    <div class="section section-2">
+        <div class="section-title">
+            <div class="section-title-line"></div>
+            <div class="section-title-text">AVAILABLE NOW</div>
+            <div class="section-title-line"></div>
+        </div>
+    </div>
+    <div class="section section-3">
+
+    </div>
 </div>
